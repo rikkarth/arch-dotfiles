973a35f1116a701a3ec7c5a70721f6cae54d46a1
rikkarth <ricardominamendes@gmail.com>
2024-05-30 03:18:02 +0100

    adjusted nvim config structure

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -25,7 +25,17 @@ local plugins = {
         'nvim-telescope/telescope.nvim', tag = '0.1.6',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
+    }
 }
 local opts = {}
 
