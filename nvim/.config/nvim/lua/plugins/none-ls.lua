b04b7e8ba9ffa0c51cade2e47d6d900e95715a25
rikkarth <ricardominamendes@gmail.com>
2024-05-31 00:33:52 +0100

    add java formats and linters

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -4,7 +4,13 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
+				null_ls.builtins.formatting.google_java_format,
+				null_ls.builtins.diagnostics.checkstyle.with({
+					extra_args = { "-c", "/google_checks.xml" },
+				}),
 				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.diagnostics.erb_lint,
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
