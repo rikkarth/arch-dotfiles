e0f33e52510798c49fdcaf5bab11eedd8fe31c07
rikkarth <ricardominamendes@gmail.com>
2024-05-30 10:32:57 +0100

    add i3, changes to nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -18,29 +18,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- installs plugins in lazy
-local plugins = {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.6',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        }
-    }
-}
-local opts = {}
-
 -- loads lazy
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
 
 -- loads treesitter
 local ts_config = require("nvim-treesitter.configs")
@@ -56,6 +35,6 @@ local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {}) -- telescope fuzy find config
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {}) -- project level live grep config
 
--- loads catppuccin
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin" -- loads catppuccin colorscheme
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+
+
